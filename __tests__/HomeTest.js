95133e8466d3ae2d93f45e1490b33e24f25462d7
Dallas Koncir <dkoncir@tenable.com>
2021-02-11 00:18:06 +0000

    Remove default content on home page.

--- __tests__/HomeTest.js
+++ __tests__/HomeTest.js
@@ -5,5 +5,5 @@ import Home from '../pages/index';
 test('Check for Getting Started Text', () => {
     const { getByText } = render(<Home />);
 
-    expect(getByText('Get started by editing')).toBeInTheDocument();
+    expect(getByText('Stock Comparison')).toBeInTheDocument();
 });
