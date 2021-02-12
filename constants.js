33546f169e8eb502fc79c0921f9d793006e641a9
Dallas Koncir <dkoncir@tenable.com>
2021-02-12 16:07:33 +0000

    Add api key constant

--- constants.js
+++ constants.js
@@ -1,1 +1,5 @@
-export const API_ROUTE_BASE = `https://www.alphavantage.co/query?apikey=${process.env.AV_API_KEY}`;
+// Ideally we would only use an env variable here but to keep things simple for local testing, I have added the API key here as well
+export const AV_API_KEY = 'LKQ7049IHKY35G5N';
+export const API_ROUTE_BASE = `https://www.alphavantage.co/query?apikey=${
+    process.env.AV_API_KEY || AV_API_KEY
+}`;
