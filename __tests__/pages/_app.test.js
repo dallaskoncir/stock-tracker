7c3299d10401f1fc3598049aeb8a22a7bb98534a
Dallas Koncir <dkoncir@tenable.com>
2021-02-16 23:56:45 +0000

    Use test instead of it.

--- __tests__/pages/_app.test.js
+++ __tests__/pages/_app.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import MyApp from '../../pages/_app';
 
 describe('<MyApp />', () => {
-    it('renders correctly', () => {
+    test('renders correctly', () => {
         const MockComponent = () => <>Test Component</>;
         const pageProps = {};
         const { container } = render(<MyApp Component={MockComponent} pageProps={pageProps} />);
--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -3,7 +3,7 @@ import { render, waitFor } from '@testing-library/react';
 import Home from '../../pages/index';
 
 describe('<Home />', () => {
-    it('renders correctly', async () => {
+    test('renders correctly', async () => {
         const { container } = render(<Home />);
 
         await waitFor(() => expect(container).toMatchSnapshot());
